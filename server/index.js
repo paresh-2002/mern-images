3f286349a8959f879ebf556e6307c825b8e1ccd1
paresh-2002 <paresh.ay99099@gmail.com>
2025-01-01 14:19:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "https://mern-images-app.vercel.app"]
+    origin: process.env.FRONTEND_URL
     credentials: true,
   })
 );
